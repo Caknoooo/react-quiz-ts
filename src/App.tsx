f19e60da070339d1de1762e32908d02b5690de39
M Naufal Badruttamam <92671053+Caknoooo@users.noreply.github.com>
2023-01-19 20:47:00 +0700

    refactor: modify some code

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, { useState } from "react";
 
-function App() {
+// Components
+import QuestionCard from "./components/QuestionCard";
+
+const App = () => {
+  const [loading, setLoading] = useState(false);
+  const [questions, setQuestions] = useState([]);
+  const [number, setNumber] = useState(0);
+  const [userAnswer, setUserAnswer] = useState([]);
+  const [score, setScore] = useState(0);
+  const [gameOver, setGameOver] = useState(true);
+
+  const startTrivia = async () => {};
+
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {};
+
+  const nextQuestion = () => {};
+
   return (
     <div className="App">
-      Quiz
+      <h1>REACT QUIZ</h1>
+      <button className="start" onClick={startTrivia}>
+        Start
+      </button>
+      <p className="score">Score:</p>
+      <p>Loading Questions...</p>
+      <QuestionCard />
+
+      <button className="next" onClick={nextQuestion}>
+        Next Question
+      </button>
     </div>
   );
-}
+};
 
 export default App;
