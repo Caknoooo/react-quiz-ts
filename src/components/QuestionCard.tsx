351b8fa00e08a8db59eb4ca5dee4f790d4959c62
M Naufal Badruttamam <92671053+Caknoooo@users.noreply.github.com>
2023-01-20 17:02:00 +0700

    feat: add userAnswer UI

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -1,39 +1,49 @@
 import React from "react";
+// Types
+import { AnswerObject } from "../App";
+// Styles
+import { Wrapper, ButtonWrapper } from "./QuestionCard.styles";
 
-type props = {
+type Props = {
   question: string;
   answers: string[];
-  callback: any;
-  userAnswer: any;
+  callback: (e: React.MouseEvent<HTMLButtonElement>) => void;
+  userAnswer: AnswerObject | undefined;
   questionNr: number;
   totalQuestions: number;
 };
 
-const QuestionCard: React.FC<props> = ({
+const QuestionCard: React.FC<Props> = ({
   question,
   answers,
   callback,
   userAnswer,
   questionNr,
   totalQuestions,
-}) => {
-  return (
+}) => (
+  <Wrapper>
+    <p className="number">
+      Question: {questionNr} / {totalQuestions}
+    </p>
+    <p dangerouslySetInnerHTML={{ __html: question }} />
     <div>
-      <p className="number">
-        Question: {questionNr} / {totalQuestions}
-      </p>
-      <p dangerouslySetInnerHTML={{ __html: question }} />
-      <div>
-        {answers.map(answer => (
-          <div>
-            <button disabled={userAnswer} onClick={callback}>
-              <span dangerouslySetInnerHTML={{  __html: answer }} />
-            </button>
-          </div>
-        ))}
-      </div>
+      {answers && answers.map((answer) => (
+        <ButtonWrapper
+          key={answer}
+          correct={userAnswer?.correctAnswer === answer}
+          userClicked={userAnswer?.answer === answer}
+        >
+          <button
+            disabled={userAnswer ? true : false}
+            value={answer}
+            onClick={callback}
+          >
+            <span dangerouslySetInnerHTML={{ __html: answer }} />
+          </button>
+        </ButtonWrapper>
+      ))}
     </div>
-  )
-};
+  </Wrapper>
+);
 
 export default QuestionCard;
